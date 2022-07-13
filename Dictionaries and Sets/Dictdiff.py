5acae6ec9186f85618bb3cdc2a00e44cf0eecbb9
muralichari <murali.chari@hotmail.com>
2022-07-12 20:51:46 -0400

    Add files via upload
    Prints the difference between two dictionaries.

---
+++ Dictionaries and Sets/Dictdiff.py
@@ -0,0 +1,17 @@
+def dictdiff(d1, d2):
+    output = {}
+    all_keys = set(d1.keys())
+    all_keys.update(d2.keys())
+
+    for key in all_keys:
+        if d1.get(key) != d2.get(key):
+            output[key] = [d1.get(key), d2.get(key)]
+    print(all_keys)
+    return output
+
+
+d1 = {'a':1, 'b':2, 'c':3}
+d2 = {'a':1, 'b':2, 'd':4}
+
+
+print(dictdiff(d1,d2))
