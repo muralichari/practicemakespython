58ec19b7ec6cbaa864266d52328d5a4d7b043a73
muralichari <murali.chari@hotmail.com>
2022-07-12 20:52:33 -0400

    Add files via upload
    Rainfall information for a city by calling a web api

--- Dictionaries and Sets/rainfall.py
+++ Dictionaries and Sets/rainfall.py
@@ -1,1 +1,12 @@
+''' This script has some useless documentations'''
+rainfall = { }
 
+while True:
+    city_name = input("Enter city name: ")
+    if not city_name:
+        break
+    mm_rain = input("Enter mm rain: ")
+    rainfall[city_name] = rainfall.get(city_name, 0) + int(mm_rain)
+
+for city,rain in rainfall.items():
+    print(f"{city} : {rain}")
